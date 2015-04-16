64e9878a84d1398184e367321f9c60b0f628a0a1
Jacob Penner <jkpenner91@gmail.com>
2015-04-15 17:53:54 -0700

    Create Initial UIState Script

---
+++ Assets/Scripts/UIState.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class UIState : MonoBehaviour {
+    public abstract int StateId { get; }
+
+    private UIStateMachine _stateMachine;
+    public UIStateMachine StateMachine {
+        get { return _stateMachine; }
+    }
+
+    public virtual string GetStateName() {
+        return "UIState";
+    }
+
+    private void Awake() {
+        _stateMachine = this.gameObject.GetComponentInParent<UIStateMachine>();
+        if (_stateMachine == null) {
+            Debug.LogWarning(string.Format(
+                "[{0}]: Requires Parent GameObject to have a UIStateMachine Component attached.",
+                this.gameObject.name));
+        } else {
+            _stateMachine.AddState(this);
+        }
+    }
+}
